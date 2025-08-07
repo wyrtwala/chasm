31374a4603a7cb8449be135b5b7c9aec54806637
wyrtwala <89209191+wyrtwala@users.noreply.github.com>
2025-08-06 21:16:04 -0700

    Update chasm.asm
    gasming

--- chasm.asm
+++ chasm.asm
@@ -1,4 +1,4 @@
-include 'chasm.inc/registers.inc'
-include 'chasm.inc/data.inc'
-include 'chasm.inc/functions.inc'
-include 'chams.inc/heap.asm'
+.include 'chasm.inc/registers.inc'
+.include 'chasm.inc/data.inc'
+.include 'chasm.inc/functions.inc'
+.include 'chams.inc/heap.asm'
